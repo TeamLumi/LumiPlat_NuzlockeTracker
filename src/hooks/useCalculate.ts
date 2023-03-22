71789f349088aa5d2e8e91b80821f612c0f8946f
AarCon01 <aarcon@debian-BULLSEYE-live-builder-AMD64>
2023-03-21 23:39:49 -0600

    Fixed most of the errors with the convert_lmpt_data.py

--- src/hooks/useCalculate.ts
+++ src/hooks/useCalculate.ts
@@ -1,4 +1,4 @@
-import { calculate, Field, Move, Pokemon, Result } from '../lumi-calc/calc/dist';
+import { calculate, Field, Move, Pokemon, Result } from 'lumi-calc/calc';
 import type { StatusName } from '@smogon/calc/dist/data/interface';
 import { useCallback, useMemo } from 'react';
 import { FORBIDDEN_ITEMS, GenderCalc } from 'constants/calculator';
