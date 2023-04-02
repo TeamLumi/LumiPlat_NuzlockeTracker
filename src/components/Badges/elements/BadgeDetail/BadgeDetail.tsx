d6498da28183b92ab10fa7144b90928038e48a31
AarCon01 <aarcon@debian-BULLSEYE-live-builder-AMD64>
2023-04-02 16:07:34 -0600

    JUST WORK

--- src/components/Badges/elements/BadgeDetail/BadgeDetail.tsx
+++ src/components/Badges/elements/BadgeDetail/BadgeDetail.tsx
@@ -1,4 +1,4 @@
-import { calculate, Field, Move, Pokemon, Result } from 'lumi-calc/dist/calc/index.js';
+import { Pokemon } from 'src/lumi-calc/dist/calc/index.js';
 import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Radio from 'semantic-ui-react/dist/commonjs/addons/Radio';
