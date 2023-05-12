1720cb510474e65066a11255d231902d86390f70
AarCon <bowtiesarecool110@gmail.com>
2023-05-12 13:47:49 -0600

    Bad Eggs for non-existent images

--- src/common/PkmImage/PkmImage.tsx
+++ src/common/PkmImage/PkmImage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface PkmImageProps {
   dexNum: number;
@@ -20,7 +20,30 @@ function getImage(monsno: number, formIndex: number) {
 
 const PkmImage = ({ dexNum, form, name, shiny = false }: PkmImageProps) => {
   const baseImgSrc = 'https://raw.githubusercontent.com/TeamLumi/luminescent-team/main/static/img/';
-  const imgSrc = baseImgSrc + getImage(dexNum, form);
+  const [imgSrc, setImgSrc] = useState(baseImgSrc + getImage(dexNum, form));
+
+  const loadImage = (src: string) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => resolve(src);
+      img.onerror = () => reject(new Error(`Error loading image: ${src}`));
+      img.src = src;
+    });
+  };
+
+  useEffect(() => {
+    loadImage(imgSrc)
+      .catch((error) => {
+        console.error(error);
+        setImgSrc(baseImgSrc + 'pm0000_00_00_00_L.webp');
+      })
+      .then((src) => {
+        if (src !== imgSrc) {
+          setImgSrc(src as string);
+        }
+      });
+  }, [dexNum, form, name, shiny, imgSrc]);
+
   return (
     <div
       className={`pkm ${shiny ? 'shiny' : ''}`}
@@ -33,3 +56,4 @@ const PkmImage = ({ dexNum, form, name, shiny = false }: PkmImageProps) => {
 };
 
 export default PkmImage;
+
