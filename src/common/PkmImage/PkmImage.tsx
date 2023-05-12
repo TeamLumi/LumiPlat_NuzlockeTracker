8ca54be54c5a15198fccabbea13b37c6e8ae40ae
AarCon <bowtiesarecool110@gmail.com>
2023-05-11 23:05:17 -0600

    Added forms of mons

--- src/common/PkmImage/PkmImage.tsx
+++ src/common/PkmImage/PkmImage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface PkmImageProps {
   dexNum: number;
@@ -11,25 +11,21 @@ function getImage(monsno: number, formIndex: number) {
   const defaultMonsno = 0;
   const defaultFormIndex = 0;
 
-  // Use input value if it is not undefined or null, otherwise use default value
-  const inputMonsno = (monsno !== undefined && monsno !== null) ? monsno : defaultMonsno;
-  const inputFormIndex = (formIndex !== undefined && formIndex !== null) ? formIndex : defaultFormIndex;
+  const inputMonsno = monsno !== undefined && monsno !== null ? monsno : defaultMonsno;
+  const inputFormIndex = formIndex !== undefined && formIndex !== null ? formIndex : defaultFormIndex;
 
   const paddedMonsno = inputMonsno.toString().padStart(4, '0');
   const paddedFormIndex = inputFormIndex.toString().padStart(2, '0');
+  
   return `pm${paddedMonsno}_${paddedFormIndex}_00_00_L.webp`;
 }
 
 const PkmImage = ({ dexNum, form, name, shiny = false }: PkmImageProps) => {
-  const baseImgSrc = 'https://raw.githubusercontent.com/TeamLumi/luminescent-team/main/static/img/';
+  const baseImgSrc = 'https://raw.githubusercontent.com/TeamLumi/LumiPlat_NuzlockeTracker/src/assets/img/pkm/';
   const imgSrc = baseImgSrc + getImage(dexNum, form);
   return (
-    <div
-      className={`pkm ${shiny ? 'shiny' : ''}`}
-      role="img"
-      style={{ transform: 'scale(1.25)' }}
-    >
-      <img src={imgSrc} style={{ maxWidth: '56px', maxHeight: '56px' }} />
+    <div className={`pkm ${shiny ? 'shiny' : ''}`} role="img" style={{ transform: 'scale(1.25)' }}>
+      <img src={imgSrc} alt={name} style={{ maxWidth: '56px', maxHeight: '56px' }} />
     </div>
   );
 };
